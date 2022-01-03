d4d73dc94c27bc26a93775dcc40efa9e984c65a8
hasnaine ahmed dihan <56039162+dihanahmed@users.noreply.github.com>
2022-01-03 23:44:00 +0600

    grabbed the company name and skills

--- beautiful_soup_for_real_website/main.py
+++ beautiful_soup_for_real_website/main.py
@@ -6,6 +6,15 @@ html_text=requests.get('https://www.timesjobs.com/candidate/job-search.html?sear
 
 soup =BeautifulSoup(html_text, 'lxml')
 
-jobs = soup.find_all('li',class_='clearfix job-bx wht-shd-bx')
+jobs = soup.find('li',class_='clearfix job-bx wht-shd-bx')
 
-print(jobs)
+#print(jobs)
+
+company_name=jobs.find('h3', class_='joblist-comp-name').text.replace(' ','')
+
+skills= jobs.find('span',class_='srp-skills').text.replace(' ','')
+
+
+print(company_name)
+
+print(skills)
