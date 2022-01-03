7873ea7fda28b35b9d097e0f04cc2c40510f048d
hasnaine ahmed dihan <56039162+dihanahmed@users.noreply.github.com>
2022-01-03 23:20:03 +0600

    the library requests has been installed and being used

---
+++ beautiful_soup_for_real_website/main.py
@@ -0,0 +1,5 @@
+from bs4 import BeautifulSoup
+import requests
+
+html_text=requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=')
+print(html_text)
