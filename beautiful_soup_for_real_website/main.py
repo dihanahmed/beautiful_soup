c16ceb69c97837f869f6285ea63868778d9d088d
hasnaine ahmed dihan <56039162+dihanahmed@users.noreply.github.com>
2022-01-03 23:30:22 +0600

    find jobs using class

--- beautiful_soup_for_real_website/main.py
+++ beautiful_soup_for_real_website/main.py
@@ -2,4 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 
 html_text=requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
-print(html_text)
+#print(html_text)
+
+soup =BeautifulSoup(html_text, 'lxml')
+
+jobs = soup.find_all('li',class_='clearfix job-bx wht-shd-bx')
+
+print(jobs)
