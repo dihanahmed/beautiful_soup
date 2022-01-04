b88a492d911b5efb717c3fabd6de05b531481423
hasnaine ahmed dihan <56039162+dihanahmed@users.noreply.github.com>
2022-01-04 12:42:46 +0600

    using time loop and time interval

--- beautiful_soup_for_real_website/main.py
+++ beautiful_soup_for_real_website/main.py
@@ -1,38 +1,47 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
-html_text=requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
-#print(html_text)
+def find_jobs():
+    html_text=requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+    #print(html_text)
 
-print('put some skills you are not familiar with')
-unfamiliar_skill=input('>')
-print(f'Filtering out:{unfamiliar_skill}')
+    print('put some skills you are not familiar with')
+    unfamiliar_skill=input('>')
+    print(f'Filtering out:{unfamiliar_skill}')
 
-soup =BeautifulSoup(html_text, 'lxml')
+    soup =BeautifulSoup(html_text, 'lxml')
 
-jobs = soup.find_all('li',class_='clearfix job-bx wht-shd-bx')
+    jobs = soup.find_all('li',class_='clearfix job-bx wht-shd-bx')
 
-#print(jobs)
-for job in jobs:
+    #print(jobs)
+    for job in jobs:
 
-    job_published_date=job.find('span',class_='sim-posted').text
-    if 'few' in job_published_date:
-        company_name=job.find('h3', class_='joblist-comp-name').text
+        job_published_date=job.find('span',class_='sim-posted').text
+        if 'few' in job_published_date:
+            company_name=job.find('h3', class_='joblist-comp-name').text
 
-        skills= job.find('span',class_='srp-skills').text.replace(' ','')
-        
-        more_info=job.header.h2.a['href']
+            skills= job.find('span',class_='srp-skills').text.replace(' ','')
+            
+            more_info=job.header.h2.a['href']
 
 
-        # print(company_name)
+            # print(company_name)
 
-        # print(skills)
+            # print(skills)
 
-        if unfamiliar_skill not in skills:
+            if unfamiliar_skill not in skills:
 
-            print(f"Company name:{company_name.strip()}")
-            print(f"Required Skills:{skills.strip()}")
-            print(f"Released Date:{job_published_date.strip()}")
-        
-            print(f'More Info:{more_info}')
-            print('')
+                print(f"Company name:{company_name.strip()}")
+                print(f"Required Skills:{skills.strip()}")
+                print(f"Released Date:{job_published_date.strip()}")
+            
+                print(f'More Info:{more_info}')
+                print('')
+
+if __name__== '__main__':
+    while True:
+        find_jobs()
+        time_wait=15
+        print(f'Waiting {time_wait} seconds ...')
+        time.sleep(time_wait)
